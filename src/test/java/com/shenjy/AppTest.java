bfcadc66d8dd635a557c297c8368a426a2547fe1
shenjiayun <shenjiayun@hzyushi.cn>
2019-07-08 14:13:07 +0800

    feat: 去除测试

--- src/test/java/com/shenjy/AppTest.java
+++ src/test/java/com/shenjy/AppTest.java
@@ -1,16 +1,6 @@
 package com.shenjy;
 
-import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobParameters;
-import org.springframework.batch.core.JobParametersBuilder;
-import org.springframework.batch.core.JobParametersInvalidException;
-import org.springframework.batch.core.launch.JobLauncher;
-import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
-import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
-import org.springframework.batch.core.repository.JobRestartException;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -23,17 +13,4 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 public class AppTest {
 
-    @Autowired
-    private Job job;
-
-    @Autowired
-    private JobLauncher jobLauncher;
-
-    @Test
-    public void testBatch() throws JobParametersInvalidException, JobExecutionAlreadyRunningException, JobRestartException, JobInstanceAlreadyCompleteException {
-        JobParameters parameters = new JobParametersBuilder()
-                .addString("JobID", String.valueOf(System.currentTimeMillis()))
-                .toJobParameters();
-        jobLauncher.run(job, parameters);
-    }
 }
